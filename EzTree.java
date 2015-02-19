b7f4e1daf5659ce117ac7d425bbd4bca66c94a96
dersmith <derekjsmith@mail.com>
2015-02-19 11:48:15 -0700

    Create EzTree.java

---
+++ EzTree.java
@@ -0,0 +1,21 @@
+/*
+ * EzTree by Derek Smith 2011
+ *    derek@ejsweb.biz
+ */
+package EzTree;
+
+/**
+ *
+ * @author Derek Smith
+ * @version 3.0.0.4
+ */
+
+public class EzTree {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        View view = new View();
+    }
+}
