c753ecf073337ff7bac8e34a0eb4adc0fcf5f85e
Derek Smith <derekjsmith@mail.com>
2015-02-20 16:28:20 -0700

    Update Yesnobox.java

--- Yesnobox.java
+++ Yesnobox.java
@@ -1,7 +1,18 @@
-/*
- * EzTree by James Smith 2011
- *    james@ejsweb.biz
- */
+/**
+   Copyright 2015 Treeconverter.com
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+**/
 package EzTree;
 import javax.swing.*;
 import java.awt.*;
@@ -10,7 +21,7 @@ import javax.swing.border.MatteBorder;
 
 /**
  *
- * @author James Smith
+ * @author Derek Smith
  */
 public class Yesnobox extends JDialog{
     private String Issue;
